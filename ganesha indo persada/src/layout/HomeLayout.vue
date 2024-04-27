4e742339e6f8b951ca72b370f044303672aadbf1
XFusion <bima.fahimna51@gmail.com>
2024-04-27 19:51:15 +0700

    feat: add new components

---
+++ ganesha indo persada/src/layout/HomeLayout.vue
@@ -0,0 +1,49 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <header class="header sticky top-0 bg-white shadow-md flex items-center justify-between px-8 py-02">
+    <!-- logo -->
+    <h1 class="w-3/12">
+        <div class="inline p-4 border-b-2 border-green-500 border-opacity-0 hover:border-opacity-100 hover:text-green-500 duration-200 cursor-pointer active">
+            Eskpor Kopi
+        </div>
+    </h1>
+
+    <!-- navigation -->
+    <nav class="nav font-semibold text-lg">
+        <ul class="flex items-center">
+            <li class="p-4 border-b-2 border-green-500 border-opacity-0 hover:border-opacity-100 hover:text-green-500 duration-200 cursor-pointer active">
+              <a href="">Accueil</a>
+            </li>
+            <li class="p-4 border-b-2 border-green-500 border-opacity-0 hover:border-opacity-100 hover:text-green-500 duration-200 cursor-pointer">
+              <a href="">Produits</a>
+            </li>
+            <li class="p-4 border-b-2 border-green-500 border-opacity-0 hover:border-opacity-100 hover:text-green-500 duration-200 cursor-pointer">
+              <a href="">Collections</a>
+            </li>
+            <li class="p-4 border-b-2 border-green-500 border-opacity-0 hover:border-opacity-100 hover:text-green-500 duration-200 cursor-pointer">
+              <a href="">Contact</a>
+            </li>
+        </ul>
+    </nav>
+
+    <!-- buttons --->
+    <div class="w-3/12 flex justify-end">
+        <a href="">
+        </a>
+        <a href="">
+        </a>
+    </div>
+</header>
+</template>
+
+<style scoped>
+</style>
