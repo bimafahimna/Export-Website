9e9a71ee535a220ee6815ac6fbebcc1e3a052d1b
Bima Fahimna <bima.fahimna51@gmail.com>
2024-05-17 12:36:40 +0700

    feat: nav bar style

--- ganesha indo persada/src/layout/HomeLayout.vue
+++ ganesha indo persada/src/layout/HomeLayout.vue
@@ -12,7 +12,7 @@ const count = ref(0)
   <header class="header sticky top-0 bg-white shadow-md flex items-center justify-between px-8 py-02">
     <!-- logo -->
     <h1 class="w-3/12">
-        <div class="inline p-4 border-b-2 border-green-500 border-opacity-0 hover:border-opacity-100 hover:text-green-500 duration-200 cursor-pointer active">
+        <div class="font-merriweather text-xl inline p-4 border-b-2 border-green-500 border-opacity-0 hover:border-opacity-100 hover:text-green-500 duration-200 cursor-pointer active">
             Eskpor Kopi
         </div>
     </h1>
@@ -20,16 +20,16 @@ const count = ref(0)
     <!-- navigation -->
     <nav class="nav font-semibold text-lg">
         <ul class="flex items-center">
-            <li class="p-4 border-b-2 border-green-500 border-opacity-0 hover:border-opacity-100 hover:text-green-500 duration-200 cursor-pointer active">
-              <a href="">Accueil</a>
+            <li class="p-4 text-xl border-b-2 border-green-500 border-opacity-0 hover:border-opacity-100 hover:text-green-500 duration-200 cursor-pointer active">
+              <a href="">Home</a>
             </li>
-            <li class="p-4 border-b-2 border-green-500 border-opacity-0 hover:border-opacity-100 hover:text-green-500 duration-200 cursor-pointer">
-              <a href="">Produits</a>
+            <li class="p-4 text-xl border-b-2 border-green-500 border-opacity-0 hover:border-opacity-100 hover:text-green-500 duration-200 cursor-pointer">
+              <a href="">Products</a>
             </li>
-            <li class="p-4 border-b-2 border-green-500 border-opacity-0 hover:border-opacity-100 hover:text-green-500 duration-200 cursor-pointer">
+            <li class="p-4 text-xl border-b-2 border-green-500 border-opacity-0 hover:border-opacity-100 hover:text-green-500 duration-200 cursor-pointer">
               <a href="">Collections</a>
             </li>
-            <li class="p-4 border-b-2 border-green-500 border-opacity-0 hover:border-opacity-100 hover:text-green-500 duration-200 cursor-pointer">
+            <li class="p-4 text-xl border-b-2 border-green-500 border-opacity-0 hover:border-opacity-100 hover:text-green-500 duration-200 cursor-pointer">
               <a href="">Contact</a>
             </li>
         </ul>
